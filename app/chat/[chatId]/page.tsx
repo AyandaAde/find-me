c344dfba4c9b7f0bc5c46bfa69d02dfb89acd958
Ayanda Adegbola <ayandakinyambo27@gmail.com>
2024-01-07 18:05:35 +0200

    hide scrollbar

--- app/chat/[chatId]/page.tsx
+++ app/chat/[chatId]/page.tsx
@@ -27,14 +27,14 @@ const ChatPage = async ({ params: { chatId } }: Props) => {
     }
     const currentChat = _chats.find(chat => chat.id === parseInt(chatId));
     return (
-        <div className="flex max-h-screen overflow-y-scroll">
-            <div className="flex w-full max-h-screen overflow-y-scroll">
+        <div className="flex max-h-screen overflow-y-scroll hide-scrollbar">
+            <div className="flex w-full max-h-screen overflow-y-scroll hide-scrollbar">
                 {/* Chat sidebar */}
                 <div className="flex-[1] max-w-xs">
                     <ChatSideBar chats={_chats} chatId={parseInt(chatId)} />
                 </div>
                 {/* pdf viewer */}
-                <div className="max-h-screen p-4 overflow-y-scroll flex-[5]">
+                <div className="max-h-screen p-4 overflow-y-scroll hide-scrollbar flex-[5]">
                     <PDFViewer pdf_url={currentChat?.pdfUrl || ""} />
                 </div>
                 {/* chat component */}
