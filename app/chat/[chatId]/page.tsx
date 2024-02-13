a2814eb1d12f355361d103369ac59751e0a6b23c
Ayanda Adegbola <ayandakinyambo27@gmail.com>
2024-02-13 16:43:22 +0200

    bg change

--- app/chat/[chatId]/page.tsx
+++ app/chat/[chatId]/page.tsx
@@ -28,7 +28,7 @@ const ChatPage = async ({ params: { chatId } }: Props) => {
     const currentChat = _chats.find(chat => chat.id === parseInt(chatId));
     return (
         <>
-            <div className="flex w-full h-full bg-gradient-to-tr from-purple-200 via-purple-400 to-purple-800 justify-center lg:hidden">
+            <div className="flex w-screen h-screen bg-gradient-to-tr from-purple-200 via-purple-400 to-purple-800 justify-center lg:hidden">
                 <h1 className="text-3xl md:text-5xl text-center font-semibold">Please use on Deskop.</h1>
             </div>
             <div className="flex max-h-screen overflow-y-scroll hide-scrollbar">
