35e3f1b6bbea6fb16bd5ac5df807e1ae14743fee
Ayanda Adegbola <ayandakinyambo27@gmail.com>
2024-02-13 16:46:45 +0200

    bg change

--- app/page.tsx
+++ app/page.tsx
@@ -19,7 +19,7 @@ export default async function Home() {
   }
   return (
     <>
-      <div className="flex w-screen h-screen bg-gradient-to-tr from-purple-200 via-purple-400 to-purple-800 justify-center lg:hidden">
+      <div className="flex w-screen h-screen bg-gradient-to-tr from-purple-200 via-purple-400 to-purple-800 justify-center items-center lg:hidden">
         <h1 className="text-3xl md:text-5xl text-center font-semibold">Please use on Deskop.</h1>
       </div>
       <div className="hidden lg:block w-screen min-h-screen bg-gradient-to-tr from-purple-200 via-purple-400 to-purple-800">
--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -3,7 +3,7 @@ import { SignUp } from "@clerk/nextjs";
 export default function Page() {
     return (
         <>
-            <div className="flex w-screen h-screen bg-gradient-to-tr from-purple-200 via-purple-400 to-purple-800 justify-center lg:hidden">
+            <div className="flex w-screen h-screen bg-gradient-to-tr from-purple-200 via-purple-400 to-purple-800 justify-center items-center lg:hidden">
                 <h1 className="text-3xl md:text-5xl text-center font-semibold">Please use on Deskop.</h1>
             </div>
             <div className="hidden lg:block absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
